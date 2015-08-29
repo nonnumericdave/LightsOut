4ffe2851a1593ae20ed886da383819f60cb92d58
David Flores <nonnumericdave@gmail.com>
2015-08-29 15:47:24 -0600

    + Introduce RecognizeLightsOutBoardStateFromImage(...), which we will eventually use to retrieve "Lights Out" board state from an image, if the game board is "found."
    -- daf

---
+++ LightsOutRecognizerPerformPlugin/DAFLightsOutRecognizer.cpp
@@ -0,0 +1,16 @@
+//
+//  DAFLightsOutRecognizer.cpp
+//  LightsOut
+//
+//  Created by David Flores on 8/29/15.
+//  Copyright (c) 2015 David Flores. All rights reserved.
+//
+
+#include "DAFLightsOutRecognizer.h"
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+bool
+DAF::RecognizeLightsOutBoardStateFromImage(const cv::Mat& kmatImage)
+{
+	return false;
+}
---
+++ LightsOutRecognizerPerformPlugin/DAFLightsOutRecognizer.h
@@ -0,0 +1,18 @@
+//
+//  DAFLightsOutRecognizer.h
+//  LightsOut
+//
+//  Created by David Flores on 8/29/15.
+//  Copyright (c) 2015 David Flores. All rights reserved.
+//
+
+#ifndef DAFLightsOutRecognizer_h
+#define DAFLightsOutRecognizer_h
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+namespace DAF
+{
+	bool RecognizeLightsOutBoardStateFromImage(const cv::Mat& kmatImage);
+};
+
+#endif
