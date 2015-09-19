10003b08e379fd693570920c40df8ad34a51b6f9
David Flores <nonnumericdave@gmail.com>
2015-09-18 23:38:15 -0600

    + Rename LightsOutViewer to LightsOutFormViewer, in order to recognize that this is the Form-based version of the product.  A pure Core Animation version will be forthcoming.
    -- daf

--- LightsOutFormViewer/AppDelegate.h
+++ LightsOutFormViewer/AppDelegate.h
@@ -1,6 +1,6 @@
 //
 //  AppDelegate.h
-//  LightsOutViewer
+//  LightsOut
 //
 //  Created by David Flores on 8/28/15.
 //  Copyright (c) 2015 David Flores. All rights reserved.
--- LightsOutFormViewer/main.m
+++ LightsOutFormViewer/main.m
@@ -1,6 +1,6 @@
 //
 //  main.m
-//  LightsOutViewer
+//  LightsOut
 //
 //  Created by David Flores on 8/28/15.
 //  Copyright (c) 2015 David Flores. All rights reserved.
